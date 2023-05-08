9ff4d0878b54526724f5735b86b75085916d9e26
jus1234 <osjamg1996@gmail.com>
2023-05-08 22:18:35 +0900

    Project init

---
+++ src/main/java/com/jscode/backendstudy/BackEndStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jscode.backendstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndStudyApplication.class, args);
+    }
+
+}
